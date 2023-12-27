ed4a233b47c8fe0518c74f4e28e8a239d65f2ecf
Ganiyu Hassan Taiwo <hassantaiwo554@gmail.com>
2023-12-27 18:15:53 +0100

    first commit

---
+++ slider.js
@@ -0,0 +1,42 @@
+const bgImageUrls = ["https://plus.unsplash.com/premium_photo-1675827055694-010aef2cf08f?q=80&w=1012&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "https://images.unsplash.com/photo-1572859704906-ab0716da285f?q=80&w=1035&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "https://images.unsplash.com/photo-1509610696553-9243c1e230f0?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "https://images.unsplash.com/photo-1511936606692-5e0d73f6b638?q=80&w=1035&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "https://images.unsplash.com/photo-1565138146061-e29b079736c0?q=80&w=927&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "https://images.unsplash.com/photo-1604069871151-23761eebcb7d?q=80&w=1064&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "https://images.unsplash.com/photo-1588842367425-8270e4b3002c?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "https://images.unsplash.com/photo-1645389411992-e9ec49b158e6?q=80&w=1036&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "https://images.unsplash.com/photo-1648198835769-96dd560071b8?q=80&w=1065&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "https://images.unsplash.com/photo-1571847665142-c7747c3d0f5f?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "https://images.unsplash.com/photo-1571847664654-6ecc1959d47c?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "https://images.unsplash.com/photo-1571847664279-c787ccddb899?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "https://images.unsplash.com/photo-1628345851631-db863f35fa0e?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "https://images.unsplash.com/photo-1613728913341-8f29b02b8253?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"];
+
+let currentImageIndex = 0;
+
+const sliderContainer = document.querySelector(".slider-container");
+sliderContainer.style.backgroundImage = `url(${bgImageUrls[currentImageIndex]})`;
+
+// previous button
+const previousE1 = document.querySelector(".previous");
+
+previousE1.addEventListener("click", ()=> {
+  previousE1.style.transform = "translateX(-20px)";
+  setTimeout(() => {
+    previousE1.style.transform = "translateX(0)";
+  }, 300);
+
+  currentImageIndex--;
+  if (currentImageIndex < 0) {
+    currentImageIndex = bgImageUrls.length;
+  }
+  sliderContainer.style.backgroundImage = `url(${bgImageUrls[currentImageIndex]})`;
+});
+
+
+//next button
+
+const nextE1 = document.querySelector(".next");
+
+nextE1.addEventListener("click", ()=> {
+  nextE1.style.transform = "translateX(20px)";
+  setTimeout(() => {
+    nextE1.style.transform = "translateX(0)";
+  }, 300);
+
+  currentImageIndex++;
+  if (currentImageIndex >= bgImageUrls.length) {
+    currentImageIndex = 0;
+  }
+  sliderContainer.style.backgroundImage = `url(${bgImageUrls[currentImageIndex]})`;
+});
+
+
